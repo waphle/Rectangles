e387083ba1b7438f88d52492b6c77d2f666951d6
Jeff Zhang <jz1766american.edu>
2020-03-28 21:15:30 -0400

    Update rectangles.js

--- rectangles.js
+++ rectangles.js
@@ -3,7 +3,7 @@
 //   - rectangles randomly are colored when the color button is clicked
 //   - when the select is changed the number of rectangles changes to match it
 
-// in the next class we will make the move function work 
+// in the next class we will make the move function work
 
 // here is a road map of the functions you need to implement.
 
@@ -26,7 +26,7 @@
 	};
 
 	// creates the number of rectangles specified in the select.
-	function createRectangles() { 
+	function createRectangles() {
 		document.getElementById("rectanglearea").innerHTML = "";
 		var count = document.getElementById("count").value;
 		// finish the functions here
@@ -46,10 +46,35 @@
 		var rects = document.querySelectorAll("#rectanglearea .rectangle");
 		var area = document.getElementById("rectanglearea");
 		for(var i = 0; i < rects.length; i++) {
-	
+
 		}
 	}
 
+	function init() {
+			id("animate-btn").addEventListener("click", animateText);
+			id("reset-btn").addEventListener("click", reset);
+	}
+
+	function animateText() {
+		if (timeId === null) {
+			id("input-text").disabled = true;
+			timerId = setInterval(nextCharacter, 500);
+		}
+		else {
+			clearInterval(timerID);
+			timerId = null;
+		}
+	}
+
+	// Resets the animation, which clears the input and output, along with the timer
+	function reset() {
+		clearInterval(timerId);
+		timeId = null;
+		index = 0;
+		id("output").textContent = "";
+		id("input-text").value = "";
+		id("input-text").disabled = false;
+	}
 
 
 })();
